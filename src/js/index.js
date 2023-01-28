74c664e3033504da1cdf7a683ed6d123987e2dce
djimenezchocron <djimenezweb@gmail.com>
2023-01-28 22:50:29 +0100

    No funciona

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
-const accordionElement = document.getElementById('accordion');
-
-accordionElement.addEventListener('click', ev => {
-  console.log(ev.target.parentElement.parentElement.children[1]);
-  ev.target.parentElement.parentElement.children[1].classList.add('.accordion__body--show');
+document.querySelectorAll('.accordion__item').forEach(item => {
+  item.addEventListener('click', () => {
+    item.children[1].classList.toggle('.accordion__body--show');
+  });
 });
+
+// const accordionElement = document.getElementById('accordion');
+
+// accordionElement.addEventListener('click', ev => {
+//   console.log(ev.target.parentElement.parentElement.children[1]);
+//   ev.target.parentElement.parentElement.children[1].classList.add('.accordion__body--show');
+// });
