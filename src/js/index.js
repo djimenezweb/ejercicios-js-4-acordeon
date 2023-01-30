8cfe8abc8751ff49e2f052c0399d82f5c9099416
djimenezchocron <djimenezweb@gmail.com>
2023-01-30 22:17:54 +0100

    Terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,12 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
-document.querySelectorAll('.accordion__item').forEach(item => {
-  item.addEventListener('click', () => {
-    item.children[1].classList.toggle('accordion__body--show');
-  });
+const accordionElement = document.getElementById('accordion');
+
+accordionElement.addEventListener('click', ev => {
+  accordionElement.querySelectorAll('.accordion__body').forEach(item => (item.style.maxHeight = 0));
+
+  const element = ev.target.children[1];
+  element.style.maxHeight = element.scrollHeight + 'px';
+
+  console.dir(element);
 });
-
-// const accordionElement = document.getElementById('accordion');
-
-// accordionElement.addEventListener('click', ev => {
-//   console.log(ev.target.parentElement.parentElement.children[1]);
-//   ev.target.parentElement.parentElement.children[1].classList.add('.accordion__body--show');
-// });
